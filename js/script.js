b8eeb0be1f6bd2566628a8a1003aadfa0608658f
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-10-31 09:54:22 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,59 +1,26 @@
 /*
-* Treehouse Techdegree:
-* FSJS Project 5 - Public API Requests
-* script js */
-
+ * Treehouse Techdegree:
+ * FSJS Project 5 - Public API Requests
+ * script js */
 
 /// var I'm working with.
 let employees = [];
-const gallery = document.querySelector('#gallery');
-const body = document.querySelector('body');
-
-///** built from code in Tree House 'Working with Fetch API Workshop' */
-// parameter: url
-async function fetchData(url) {
-    try {
-        const response = await fetch(url);
-        const res = await checkStatus(response);
-        return res.json();
-    } catch (e) {
-        return console.log('Problem', e);
-    }
-}
-
-//  getting 12 random users from the Random User API source 
-fetchData('https://randomuser.me/api/?results=12&nat=us')
-    .then(data => {
-        employees = data.results
-        employees.forEach(employee => {
-            const picture = `${employee.picture.large}`
-            const name = `${employee.name.first} ${employee.name.last}`
-            const email = `${employee.email}`
-            const location = `${employee.location.city}, ${employee.location.state}`
-            
-            displayCard(name, email, location, picture) 
-        })
-    })
+const gallery = document.querySelector("#gallery");
+const body = document.querySelector("body");
+const url = "https://randomuser.me/api/?results=12&nat=us";
 
 
-/// Handling potential errors.
-///** code from Tree House 'Working with Fetch API Workshop' */
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
-    }
-}
+fetch(url)
+  .then((response) => response.json()) ///reads res and returns promise that resolves the json 
+  .then((response) => buildEmployees(response.results));
+ /// 1 func for card and one for model
 
 
-///
-/* Helper Functions */
-///
 
-/// displays employee information
- function displayCard(name, email, location, picture) {
-    const ehtml = `
+// helper function 
+// displays employee information
+function displayCard(name, email, location, picture) {
+  const ehtml = `
         <div class="card">
             <div class="card-img-container">
                 <img class="card-img" src=${picture} alt="profile picture">
@@ -65,13 +32,14 @@ function checkStatus(response) {
             </div>
         </div>
      `;
-    gallery.insertAdjacentHTML('beforeend', ehtml)
+  gallery.insertAdjacentHTML("beforeend", ehtml);
 }
 
-/// Modal to display when employee is selected.
-/// employee (e)html string is added to the <body><body>
+
+// Modal to display when employee is selected.--
+// employee (e)html string is added to the <body><body>
 function displayModal(picture, name, email, city, number, address, birthday) {
-    const ehtml = `
+  const ehtml = `
         <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -89,64 +57,107 @@ function displayModal(picture, name, email, city, number, address, birthday) {
         </div>
     `;
 
-    body.insertAdjacentHTML('beforeend', ehtml)
+  body.insertAdjacentHTML("beforeend", ehtml);
 }
 
-/// formating for paramaters: dob: MM/DD/YYYY, phone (XXX) XXX-XXXX, location.
-/// used Regex and template literals.
-function birthdate(dob) {
-    let birthday = dob.date.substr(0,10);
-    const regex = /(\d{4})-(\d{2})-(\d{2})/;
-    return birthday.replace(regex, '$2/$3/$1')
-}
+function buildEmployees(employeeData){
+    // console.log(employeeData);
+    employeeData.forEach(aEmployee => {
+        displayCard(`${aEmployee.name.first} ${aEmployee.name.last}`, aEmployee.email, aEmployee.location.city , aEmployee.picture.medium);  
+        // console.log(displayCard);
+    });
+ }
+ 
 
-function phoneNumber(phone) {
-    const regex = /^\D*(\d{3})\D*(\d{3})-\D*(\d{4})\D*$/;
-    return phone.replace(regex, '($1) $2-$3')
-}
+ ///Setup- Modal Elements 
+//  const modelButton = getElementById("modal-close-btn");
+ const modalDiv = document.getElementsByClassName(".modal");
+ const card = document.querySelector(".card");
 
-function addresss(location) {
-    return `${location.street.number} ${location.street.name}, ${location.city}, ${location.state} ${location.postcode}`
-}
 
-/**************/
 
-function checkClickedEmployee (path, name) {
-    let correct = false;     //returns T/F
 
-    path.forEach(element => {
-        if (element.className === 'card') {
-            if(element.childNodes[3].childNodes[1].textContent === name) {
-                correct = true;
-            }
-        } 
-    })
-    return correct;
-}
+//  function todo (){
+//    gallery.addEventListener("click", (e) => {
+//     card.style.display = "block";
+//    }
 
-/// Event listener responds tp emplyee name 'click'
-// .foreach() loops through employee [array]. Modal called if information matches. 
-gallery.addEventListener('click', (e) => {
+//    );
+//  }
+
+
+
+
+
+
+//  //listen for open 
+//  modelButton.addEventListener('click', (e) 
+//  function c displayCard(){
+//     modalDiv.style.display = "block";}
+//  )
+
+//  gallery.addEventListener("click",(e) => {
+//     const card = document.querySelector(".card");
+//     console.log();
+//         if (card.clicked === true) {
+           
+//         }
+        
+       
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // const modal = document.querySelector(".modal-container");
+        // modal.style.display = 'none';
+        // // if (card === 'X'){
     
-    employees.forEach(employee => {
-        const picture = `${employee.picture.large}`
-        const name = `${employee.name.first} ${employee.name.last}`
-        const email = `${employee.email}`
-        const city = `${employee.location.city}`
-        const number = phoneNumber(employee.phone)
-        const address = addresss(employee.location)
-        const birthday = birthdate(employee.dob)
+    
+ 
+ 
+//  gallery.addEventListener("click",(e) =>{
+//     if (e.target !== gallery)
+//     // const modal = document.querySelector(".modal-container");
+//     // modal.style.display = 'none';
+//     // // if (card === 'X'){
 
-        if (checkClickedEmployee(e.composedPath(), name)) {
-            displayModal(picture, name, email, city, number, address, birthday)
-        }
-    })
-})
+//     }
 
-/// Event listener for the close button
-body.addEventListener('click', (e) => {
-    if (e.target.textContent === 'X') {
-        var modalContainer = document.querySelector('.modal-container');
-        modalContainer.parentNode.removeChild(modalContainer);
-    }
-})
+//  );
+// gallery.addEventListener("click",(e) =>{
+//     const card = document.querySelector(".card");
+// if (con) {
+//     displayCard();
+
+// }
+// console.log(con);
+// });
+
+
+// // Event listener for the close button
+// body.addEventListener("click", (e) => {
+//   if (e.target.textContent === "X") {
+//     var modalContainer = document.querySelector(".modal-container");
+//     modalContainer.parentNode.removeChild(modalContainer);
+//   }
+// });
+
+// //mine
+// // Event listener for the close button
+// const x = body.addEventListener("click", (e) => {
+//     if ('click' === card) {
+//       var modalContainer = document.querySelector(".modal-container");
+//       modalContainer.parentNode.removeChild(modalContainer);
+//     }
+//   });
+//   console.log(x);

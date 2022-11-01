a38c427e8706df3f2fe7142ce75d680562f20aa8
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-10-31 21:43:55 -0700

    debugging

--- js/script.js
+++ js/script.js
@@ -9,19 +9,22 @@ const gallery = document.querySelector("#gallery");
 const body = document.querySelector("body");
 const url = "https://randomuser.me/api/?results=12&nat=us";
 
-
+//Getting and displaying 12 random users. 
 fetch(url)
   .then((response) => response.json()) ///reads res and returns promise that resolves the json 
-  .then((response) => buildEmployees(response.results));
- /// 1 func for card and one for model
+  .then((response) => {
+    buildEmployees(response.results);
+    employees = response.results;
+    // console.log(employees);
+});
 
 
-
-// helper function 
-// displays employee information
-function displayCard(name, email, location, picture) {
+// displays employee information.
+// html is appended to gallery Div.
+function displayCard(name, email, location, picture, index) {
+    // console.log(index);
   const ehtml = `
-        <div class="card">
+        <div class="card" data-index = ${index}>
             <div class="card-img-container">
                 <img class="card-img" src=${picture} alt="profile picture">
             </div>
@@ -37,21 +40,23 @@ function displayCard(name, email, location, picture) {
 
 
 // Modal to display when employee is selected.--
-// employee (e)html string is added to the <body><body>
-function displayModal(picture, name, email, city, number, address, birthday) {
+// employee (e)html string is appended to the body element. 
+function displayModal(index, employees) {
+    const data = employees[index];
+    console.log(data);
   const ehtml = `
         <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src=${picture} alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${name}</h3>
-                    <p class="modal-text">${email}</p>
-                    <p class="modal-text cap">${city}</p>
+                    <img class="modal-img" src=${data.picture.medium} alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                    <p class="modal-text">${data.email}</p>
+                    <p class="modal-text cap">${data.location.city}</p>
                     <hr>
-                    <p class="modal-text">${number}</p>
-                    <p class="modal-text">${address}</p>
-                    <p class="modal-text">${birthday}</p>
+                    <p class="modal-text">${data.cell}</p>
+                    <p class="modal-text"> ${data.location.street.number} ${data.location.street.name}</p>
+                    <p class="modal-text">${data.dob.date.substring(0, 10)}</p>
                 </div>
             </div>
         </div>
@@ -60,104 +65,41 @@ function displayModal(picture, name, email, city, number, address, birthday) {
   body.insertAdjacentHTML("beforeend", ehtml);
 }
 
+
 function buildEmployees(employeeData){
+    let counter = 0;
     // console.log(employeeData);
     employeeData.forEach(aEmployee => {
-        displayCard(`${aEmployee.name.first} ${aEmployee.name.last}`, aEmployee.email, aEmployee.location.city , aEmployee.picture.medium);  
+        displayCard(`${aEmployee.name.first} ${aEmployee.name.last}`, aEmployee.email, aEmployee.location.city , aEmployee.picture.medium, counter);  
+        counter++;
         // console.log(displayCard);
     });
+    // employees = employeeData;
+    // console.log(employees);
  }
  
 
  ///Setup- Modal Elements 
-//  const modelButton = getElementById("modal-close-btn");
- const modalDiv = document.getElementsByClassName(".modal");
  const card = document.querySelector(".card");
 
-
-
-
-//  function todo (){
-//    gallery.addEventListener("click", (e) => {
-//     card.style.display = "block";
-//    }
-
-//    );
-//  }
-
-
-
-
-
-
-//  //listen for open 
-//  modelButton.addEventListener('click', (e) 
-//  function c displayCard(){
-//     modalDiv.style.display = "block";}
-//  )
-
-//  gallery.addEventListener("click",(e) => {
-//     const card = document.querySelector(".card");
-//     console.log();
-//         if (card.clicked === true) {
-           
-//         }
-        
-       
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // const modal = document.querySelector(".modal-container");
-        // modal.style.display = 'none';
-        // // if (card === 'X'){
-    
-    
  
- 
-//  gallery.addEventListener("click",(e) =>{
-//     if (e.target !== gallery)
-//     // const modal = document.querySelector(".modal-container");
-//     // modal.style.display = 'none';
-//     // // if (card === 'X'){
+ gallery.addEventListener("click",(e) => {
+        // console.log("clicked");
+    if (e.target.closest(".card")){
+       const card =  e.target.closest(".card");
+       const index = Number(card.getAttribute("data-index"));
+    //    console.log(card.getAttribute("data-index"));
+    //    console.log(index);
+       displayModal(index, employees);
+    }        
+ });
 
-//     }
+// Event listener for the close button
+body.addEventListener("click", (e) => {
+    if (e.target.textContent === "X") {
+        const modalDiv = document.querySelector(".modal-container");
+        // console.log(modalDiv);
+     modalDiv.parentNode.removeChild(modalDiv);
+    }
+    });
 
-//  );
-// gallery.addEventListener("click",(e) =>{
-//     const card = document.querySelector(".card");
-// if (con) {
-//     displayCard();
-
-// }
-// console.log(con);
-// });
-
-
-// // Event listener for the close button
-// body.addEventListener("click", (e) => {
-//   if (e.target.textContent === "X") {
-//     var modalContainer = document.querySelector(".modal-container");
-//     modalContainer.parentNode.removeChild(modalContainer);
-//   }
-// });
-
-// //mine
-// // Event listener for the close button
-// const x = body.addEventListener("click", (e) => {
-//     if ('click' === card) {
-//       var modalContainer = document.querySelector(".modal-container");
-//       modalContainer.parentNode.removeChild(modalContainer);
-//     }
-//   });
-//   console.log(x);
